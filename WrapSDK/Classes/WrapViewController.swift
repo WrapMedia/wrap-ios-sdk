2b2666a332fee678dfa4657de86e65b293c3a784
Jay Hall <jay.hall@wrap.co>
2016-06-30 14:21:00 -0700

    replaced UIWebView with WKWebView

--- WrapSDK/Classes/WrapViewController.swift
+++ WrapSDK/Classes/WrapViewController.swift
@@ -15,11 +15,12 @@ class WrapViewController: UIViewController {
     
     public var wrapID: String?
     
-    @IBOutlet weak var webView : UIWebView!
+    var webView : WKWebView = WKWebView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.addSubview(webView)
+        configureAutolayoutConstraints()
     }
     
     override func didReceiveMemoryWarning() {
@@ -38,5 +39,26 @@ class WrapViewController: UIViewController {
         
         webView.loadRequest(request)
     }
+    
+    @IBAction func tappedDone(sender: AnyObject?) {
+        presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func configureAutolayoutConstraints()
+    {
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        
+        if #available(iOS 9.0, *) {
+            webView.topAnchor.constraintEqualToAnchor(view.topAnchor).active = true
+            webView.widthAnchor.constraintEqualToAnchor(view.widthAnchor).active = true
+            webView.bottomAnchor.constraintEqualToAnchor(view.bottomAnchor).active = true
+        } else {
+            // Fallback on earlier versions
+        }
+        
+        
+        view.layoutIfNeeded()
+    }
+
 }
 
