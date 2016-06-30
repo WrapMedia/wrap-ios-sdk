3a584e087d195cadf78de948f316a153490234ca
Francis Li <mail@francisli.com>
2016-06-30 13:57:06 -0700

    Show Profile cover image

---
+++ WrapSDK/Classes/WrapProfileCoverView.swift
@@ -0,0 +1,13 @@
+//
+//  WrapProfileCoverView.swift
+//  Pods
+//
+//  Created by Francis Li on 6/30/16.
+//
+//
+
+import UIKit
+
+class WrapProfileCoverView: UICollectionReusableView {
+        
+}
